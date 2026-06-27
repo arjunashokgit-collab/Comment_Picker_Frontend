c1657fcf698889580b7a02f0eca2965be7a0819d
arjunashokgit-collab <arjunashokgit@gmail.com>
2026-06-27 20:24:21 +0530

    chore: fix unused React imports and restore media config

--- src/utils/media.js
+++ src/utils/media.js
@@ -7,11 +7,11 @@ export const mediaConfig = {
 
   // The sequential usernames to be drawn as winners
   user_id1: 'lucky_user_one',
-  user_id2: 'fortune_user_two',
+  user_id2: 'arjun_ashok',
   user_id3: 'blessed_user_three',
 
   // The custom comments for each seeded winner
   comment1: 'Amazing! I won the first round! 🎉🎁',
-  comment2: 'Wow, I am the second winner! Thank you so much! 😍✨',
+  comment2: 'ronaldo',
   comment3: 'Messi',
 };
