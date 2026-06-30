53d5c79baa0348fde5c849f7097237b6e2027a34
arjunashokgit-collab <arjunashokgit@gmail.com>
2026-06-30 21:29:31 +0530

    updated

--- src/utils/media.js
+++ src/utils/media.js
@@ -1,10 +1,10 @@
 // This file allows configuring a custom media image and seeded winner sequence.
 // When drawing winners, the users and comments will be picked in this specific order.
-import giveawayimage from "./giveaway.png"
+import image123 from "./image.png"
 
 export const mediaConfig = {
   // The custom image to display in the frontend when creating/configuring a giveaway
-  image: giveawayimage,
+  image: image123,
 
   // The sequential usernames to be drawn as winners
   user_id1: 'methealenthomas',
