8b90094e0edd3750b4690d9977d08e980d792a15
arjunashokgit-collab <arjunashokgit@gmail.com>
2026-07-15 19:31:32 +0530

    updated

--- src/utils/media.js
+++ src/utils/media.js
@@ -1,13 +1,13 @@
 // This file allows configuring a custom media image and seeded winner sequence.
 // When drawing winners, the users and comments will be picked in this specific order.
-import image123 from "./image8.png"
+import image123 from "./image9.png"
 
 export const mediaConfig = {
   // The custom image to display in the frontend when creating/configuring a giveaway
   image: image123,
 
   // The sequential usernames to be drawn as winners
-  user_id1: 'raju_anandhhv',
+  user_id1: 'abhiiram2745',
   user_id2: 'irin_thomas124',
   user_id3: 'amal_shajan__',
   user_id4: 'vichuzzsssee',
@@ -17,8 +17,8 @@ export const mediaConfig = {
   user_id8: '_goe_ll_jo_n06',
 
   // The custom comments for each seeded winner
-  comment1: 'Argentina 🇦🇷',
-  comment2: 'Messi 🇦🇷❤️‍🩹',
+  comment1: 'Spain 🇪🇸 ',
+  comment2: 'France 🇫🇷',
   comment3: 'L10 🐐🇦🇷',
   comment4: 'Vamoozzz',
   comment5: 'Brazil 🇧🇷',
